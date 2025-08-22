0c2cf9a1f3a853e03e68582a96d5aed2ac786a5e
ycheung5 <ycheung5@vt.edu>
2025-08-22 16:34:53 +0800

    debug

--- wordle/__init__.py
+++ wordle/__init__.py
@@ -1,2 +1,2 @@
-"""Wordle core package (Task 1)."""
-from .game import Wordle, Score, score_to_string, load_word_list
+"""Public API for the wordle package."""
+from .game import Wordle, Score, score_to_string, load_word_list, normalize_word
